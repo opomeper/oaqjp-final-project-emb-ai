79cd124baeae492c36c14ffaae75c55b55a4882f
opomeper <saurojoemer14@gmail.com>
2025-08-31 11:36:56 -0400

    Clean up code for static analysis compliance

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:8000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,11 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector')
 def sent_detector():
+    """This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the emotions and scores 
+        for the provided text, and also dominant emotion.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
@@ -22,6 +31,9 @@ def sent_detector():
 
 @app.route('/')
 def render_index_page():
+    """ This function initiates the rendering of the main application
+        page over the Flask channel
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
