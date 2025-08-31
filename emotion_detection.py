995768f1bd5b2516edd1176201cb139cf0ac5df9
opomeper <saurojoemer14@gmail.com>
2025-08-31 00:52:52 -0400

    Modify emotion_detector function to change return format

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,18 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    # extract each emotion
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    return {
+        'anger' : anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+    }
+
